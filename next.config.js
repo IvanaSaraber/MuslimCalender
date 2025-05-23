8ca79fdf1322349ee9646e8c89e1189bb3349b41
IvanaSaraber <irsaraber@gmail.com>
2025-05-23 21:58:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
