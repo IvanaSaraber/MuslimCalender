75a90e0ac505be3299b790a745b4517b16c18906
IvanaSaraber <irsaraber@gmail.com>
2025-05-25 22:05:24 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Islamic Events App",
+  description: "Overzicht van islamitische evenementen in Nederland",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="nl">
+      <body>{children}</body>
+    </html>
+  );
+}
