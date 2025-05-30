177eae9d90e0cc2e6ebae90ee2918e1d87e85a73
IvanaSaraber <irsaraber@gmail.com>
2025-05-30 14:33:02 +0200

    Create airtable.ts

---
+++ lib/airtable.ts
@@ -0,0 +1,11 @@
+import Airtable from "airtable";
+
+const apiKey = process.env.AIRTABLE_API_KEY!;
+const baseId = process.env.AIRTABLE_BASE_ID!;
+const organisationTable = process.env.ORGANISATION_TABLE || "OrganisationData";
+const eventTable = process.env.EVENT_TABLE || "EventRecords";
+
+const base = new Airtable({ apiKey }).base(baseId);
+
+export const organisationTableRef = base(organisationTable);
+export const eventTableRef = base(eventTable);
