da615555597602d7c8948d97070d279103322895
IvanaSaraber <irsaraber@gmail.com>
2025-07-07 16:13:02 +0200

    Update route.ts

--- app/api/submit-event/route.ts
+++ app/api/submit-event/route.ts
@@ -1,6 +1,7 @@
 // app/api/submit-event/route.ts
 import { NextResponse } from "next/server";
-import { addEventToAirtable } from "@/lib/airtable";
+import { addEventToAirtable } from "../../../lib/airtable";
+
 
 export async function POST(req: Request) {
   try {
